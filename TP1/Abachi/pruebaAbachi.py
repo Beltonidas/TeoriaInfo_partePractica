e72535a594cd97f35bb0b86907acba0e57a8e284
Tomas E. Schuenemann <70041852+Beltonidas@users.noreply.github.com>
2022-04-02 20:10:16 -0300

    Abachi san

--- TP1/Abachi/pruebaAbachi.py
+++ TP1/Abachi/pruebaAbachi.py
@@ -2,11 +2,10 @@ from random import random
 from re import I
 
 
-def Arrojar_dado ():
-  prob_acum={1/6, 2/6, 3/6, 4/6, 5/6, 1} 
-      p= random (); 
-      X= 6
-      for (I=1 in X) 
-        if (p <prob_acum[ i ]) 
-            return i;       
-   
+def Arrojar_dado():
+    prob_acum = {1/6, 2/6, 3/6, 4/6, 5/6, 1}
+    p = random()
+    X = 6
+    for (I=1 in X)
+    if (p < prob_acum[i])
+    return i
