54ad065f8aec39735ea6a99c2548214fc394f5c2
Abachian <54768966+Abachian@users.noreply.github.com>
2022-04-03 01:16:41 -0300

    segunda subida

--- TP1/Abachi/pruebaAbachi.py
+++ TP1/Abachi/pruebaAbachi.py
@@ -1,11 +1,34 @@
+from pickle import TRUE
 from random import random
 
 
 
 def Arrojar_dado():
-    prob_acum = {1/6, 2/6, 3/6, 4/6, 5/6, 1}
+    prob_acum = [1/6, 2/6, 3/6, 4/6, 5/6, 1]
     p = random()
-    X = 6
     for i in range(6):
         if (p < prob_acum[i]): 
             return i
+
+def converge (ant, act): 
+   if ( abs (ant - act )< 0.0000000001 ):
+        return True
+   return False
+ 
+def Calcular_prob_suma6(prob_act):
+    exitos=0
+    N=2
+    prob_ant=-1
+    prob_act=0
+    minimo=0
+    while (not converge (prob_ant,prob_act) or minimo>2):
+        x= Arrojar_dado();
+        y= Arrojar_dado();
+        if(x+y==6):
+            exitos+1
+        N+1
+        prob_ant=prob_act
+        prob_act= exitos/N
+    return prob_act
+
+
