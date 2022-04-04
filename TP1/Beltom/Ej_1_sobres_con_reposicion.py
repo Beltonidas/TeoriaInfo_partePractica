d73938bd9109603e558b9bf359dee75fd3bc0760
Tomas E. Schuenemann <70041852+Beltonidas@users.noreply.github.com>
2022-04-04 11:24:37 -0300

    Pude resolver el ejercicio de sobres sin reposicion pero me da una probabilidad del 30 % nose si tiene mucho sentido

--- TP1/Beltom/Ej_1_sobres_con_reposicion.py
+++ TP1/Beltom/Ej_1_sobres_con_reposicion.py
@@ -1,14 +1,42 @@
 from random import *
-import re
 listSobres = [2/5, 1]
+
+
+def converge(act, ant):
+    print("mi valor absoluto es:", abs(ant - act))
+    if(abs(ant - act) < 0.001):
+        return True
+    return False
+
+
+def calcular_probabilidad():
+    exitos = 0
+    nTotal = 0
+    probAnterior = -1
+    probActual = 1
+    cantMinima = 10
+    while((converge(probActual, probAnterior) == False) or (nTotal < cantMinima)):
+        x = retirarSobre()
+        print("sobre 1", x)
+        y = retirarSobre()
+        print("sobre 2", y)
+        if ((x == 1) and (y == 1)):
+            exitos = exitos + 1
+            print("la cantidad de exitos: ", exitos)
+        nTotal = nTotal + 1
+        probAnterior = probActual
+        probActual = exitos/nTotal
+        print("la cantidad de exitos sobre el total es: ", exitos, " /", nTotal)
+    return probActual
+
+
 def retirarSobre():
-    n= random()
-    print (n)
-    for i in range(2):
-        print ("iterador i: ",i)
-        if n < listSobres[i]:
-            return i
+    n = random()
+    if (n > 2/5):
+        return 1
+    else:
+        return 0
 
-action = retirarSobre()
-print (action)
 
+resul = calcular_probabilidad()
+print(resul)
