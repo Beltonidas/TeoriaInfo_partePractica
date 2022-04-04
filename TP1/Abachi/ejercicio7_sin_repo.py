3f2ea5c7ea2047793b2a81f2723664f66ebdde5e
Abachian <54768966+Abachian@users.noreply.github.com>
2022-04-04 14:29:53 -0300

    casi listo sin repo

--- TP1/Abachi/ejercicio7_sin_repo.py
+++ TP1/Abachi/ejercicio7_sin_repo.py
@@ -1,7 +1,5 @@
 import random as randomLibrary
-from random import *
-n = random()
-print(n)
+from random import random
 
 
 def Sacar_primer_sobre():
@@ -13,30 +11,36 @@ def Sacar_primer_sobre():
 
 
 def Sacar_segundo_sobre(primer_sobre):
-    prob_acum = [1/2, 3/4], [1, 1]
+    prob_acum = [[1/2, 3/4], [1, 1]]
     p = random()
-    for i in range(0, 1):
+    for i in range(len(prob_acum)):
         if p < prob_acum[i][primer_sobre]:
             return i
 
 
 def converge(ant, act):
-    if (abs(ant - act) < 0.0001):
+    if (abs(ant - act) < 0.000001):
         return True
     return False
 
 
 def Sacar_al_menos_1_facil():
     exitos = 0
-    N = 0
+    n = 0
     prob_ant = -1
     prob_act = 1
-    while (not converge(prob_ant, prob_act)):
+    minimo = 0
+    while (not converge(prob_ant, prob_act) or minimo < 15):
         x = Sacar_primer_sobre()
-        y = 0
+        y = Sacar_segundo_sobre(x)
         if(x+y == 6):
-            exitos+1
-        N+1
+            exitos = exitos+1
+        n = n+1
         prob_ant = prob_act
-        prob_act = exitos / N
+        prob_act = exitos / n
+        minimo = minimo+1
     return prob_act
+
+
+prueba = Sacar_al_menos_1_facil()
+print(prueba)
