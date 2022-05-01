3c66b2ac1b24e9935f018299be838110f8c11410
Abachian <54768966+Abachian@users.noreply.github.com>
2022-05-01 17:18:51 -0300

    con problemas de indices

--- TP2/Matias/ejercicio11b.py
+++ TP2/Matias/ejercicio11b.py
@@ -1,5 +1,6 @@
 from random import *
 import numpy as np
+from operator import truediv
 #                   1               2          3
 prob_acum = [[0.5, 1, 1], [1/3, 2/3, 1], [0, 1, 1]]
 
@@ -9,12 +10,12 @@ vInicial_acum = [1/3, 2/3, 1]
 epsilon = 0.0001
 simbolos = 3
 T_MIN = 1000
-pasos = 500
+pasos = 4
 
 
-def converge(act, ant):
+def converge(act, ant, pasos, Simbolo):
     for i in range(pasos):
-        if(abs(act[i] - ant[i]) > epsilon):
+        if(abs(act[Simbolo, i] - ant[Simbolo, i]) > epsilon):
             return False
     return True
 
@@ -35,37 +36,31 @@ def second_symbol(s_ant):
 
 def Prob_primera_recurrencia():
     # retornos = recurrencia
-    pasos = 5
     T_MIN = 1000
-    retornos = np.zeros((3,pasos)) # retornos a si en n pasos
-    frecRetornos = np.zeros((3,pasos)) # matriz de retornos pero en probabilidades
-    frecuenciaActual = []  # prob. primera recurrencia actual
-    frecIncAnte = []  # prob. primera recurrencia anterior
-    total_retornos = [] # array de total de retornos por simbolo
-    ult_ret = [] # array de ultimos retornos
-    
-    for _ in range(pasos):#inicializamos arrays
-        frecuenciaActual.append(0)
-        frecIncAnte.append(-1)
-        total_retornos.append(0)
-        ult_ret.append(0)
-
-    t_actual = 0    
-    primerSimbolo = first_symbol()
-    ult_ret[primerSimbolo] = t_actual
-
-    while (t_actual < T_MIN or not converge(frecuenciaActual, frecIncAnte)):
-        NuevoSimbolo = second_symbol(primerSimbolo)
+    retornos = np.zeros((3, pasos))  # retornos a si en n pasos
+    # matriz de retornos pero en probabilidades
+    frecuenciaActual = np.zeros((3, pasos))  # prob. primera recurrencia actual
+    frecIncAnte = np.ones_like(frecuenciaActual)
+    frecIncAnte = frecIncAnte*-1  # prob. primera recurrencia anterior
+    total_retornos = [0, 0, 0]  # array de total de retornos por simbolo
+    ult_ret = [-1, -1, -1]  # array de ultimos retornos
+    t_actual = 0
+    Simbolo = first_symbol()
+    ult_ret[Simbolo] = t_actual
+    while (t_actual < T_MIN or not converge(frecuenciaActual, frecIncAnte, pasos, Simbolo)):
+        Simbolo = second_symbol(Simbolo)
         t_actual += 1
         frecIncAnte = frecuenciaActual
-        pos = t_actual - ult_ret[NuevoSimbolo]
-        if not (pos == -1):
-            retornos[NuevoSimbolo,pos] += 1
-            total_retornos[NuevoSimbolo] += 1
-        ult_ret[NuevoSimbolo] = t_actual
+        pos = t_actual - abs(ult_ret[Simbolo])
+        if not (ult_ret[Simbolo] == -1):
+            retornos[Simbolo, pos] += 1
+            total_retornos[Simbolo] += 1
+        ult_ret[Simbolo] = t_actual
         # : selecciona todas las columnas
-        frecRetornos[NuevoSimbolo,:] = retornos[NuevoSimbolo,:]/total_retornos[NuevoSimbolo]
-    return frecRetornos
+        frecuenciaActual[Simbolo, :] = retornos[Simbolo, :] / \
+            total_retornos[Simbolo]
+    return frecuenciaActual
+
 
 v = Prob_primera_recurrencia()
 print(v)
