ce1336867fddc1d0e0ccde8bc904e8d991b40057
venatorgod <santiago_faiella@hotmail.com>
2022-05-18 18:29:46 -0300

    Codigo Huffman Probar

--- TP3/Beltom/Entropia.py
+++ TP3/Beltom/Entropia.py
@@ -19,3 +19,34 @@ for i in range(len(listPi)):
 
 print("El resultado es: ", entropia)
 
+#Calculadora codigo Huffman
+lista_valores = [(0.5,[0]),(0.3,[1]),(0.1,[2]),(0.1,[3])]
+lista_codigos = []
+
+for x in range(len(lista_valores)):
+    lista_codigos.append([])
+
+def calcular_codigos(lista_valores_originales):
+    def devolverPrimero(elemento):
+        return elemento[0]
+
+    while (len(lista_valores_originales)>1):
+        lista_valores_originales.sort(key=devolverPrimero)
+
+        elemento1 = lista_valores_originales.pop(0)
+        for x in elemento1[1]:
+            lista_codigos[x].append(0)
+
+        elemento2 = lista_valores_originales.pop(0)
+        for x in elemento2[1]:
+            lista_codigos[x].append(1)
+
+        elemento_aux = []
+        elemento_aux.append(elemento1[0]+elemento2[0])
+        elemento_aux.append(elemento1[1]+elemento2[1])
+        lista_valores_originales.append(elemento_aux)
+    for x in lista_codigos:
+        x.reverse()
+
+calcular_codigos(lista_valores)
+print(lista_codigos)
